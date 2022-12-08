7d12c3a601e3add8856af6fc0efd13f05a673815
Cody <cody.koelliker@outlook.com>
2022-12-07 20:28:29 -0700

    collector base

--- src/main/java/coda/littlebeasties/common/entities/Collector.java
+++ src/main/java/coda/littlebeasties/common/entities/Collector.java
@@ -1,21 +1,15 @@
 package coda.littlebeasties.common.entities;
 
-import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.tags.FluidTags;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.npc.AbstractVillager;
 import net.minecraft.world.item.trading.MerchantOffer;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelAccessor;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.Random;
 
 public class Collector extends AbstractVillager {
 
@@ -24,7 +18,7 @@ public class Collector extends AbstractVillager {
     }
 
     public static AttributeSupplier.Builder createCollectorAttributes() {
-        return LivingEntity.createLivingAttributes().add(Attributes.MAX_HEALTH, 20.0D).add(Attributes.MOVEMENT_SPEED, 0.25D);
+        return Mob.createMobAttributes().add(Attributes.MAX_HEALTH, 20.0D).add(Attributes.MOVEMENT_SPEED, 0.25D);
     }
 
     @Override
