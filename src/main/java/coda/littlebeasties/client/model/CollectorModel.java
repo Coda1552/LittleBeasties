279a759a185a29b55253bc3e5fb18115efbbc6f6
Coda <cody.koelliker@outlook.com>
2022-12-09 17:53:29 -0700

    collector animations

--- src/main/java/coda/littlebeasties/client/model/CollectorModel.java
+++ src/main/java/coda/littlebeasties/client/model/CollectorModel.java
@@ -7,12 +7,43 @@ import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.*;
+import net.minecraft.util.Mth;
 
 public class CollectorModel extends EntityModel<Collector> {
-	private final ModelPart Body;
+	private final ModelPart body;
+	private final ModelPart rightLeg;
+	private final ModelPart leftLeg;
+	private final ModelPart tail;
+	private final ModelPart tail2;
+	private final ModelPart fin;
+	private final ModelPart rightEye;
+	private final ModelPart leftEye;
+	private final ModelPart rightAntenna;
+	private final ModelPart leftAntenna;
+	private final ModelPart rightBag;
+	private final ModelPart leftBag;
+	private final ModelPart rightArm;
+	private final ModelPart rightHand;
+	private final ModelPart leftArm;
+	private final ModelPart leftHand;
 
 	public CollectorModel(ModelPart root) {
-		this.Body = root.getChild("Body");
+		this.body = root.getChild("Body");
+		this.rightLeg = body.getChild("RightLeg");
+		this.leftLeg = body.getChild("LeftLeg");
+		this.rightAntenna = body.getChild("RightAntenna");
+		this.leftAntenna = body.getChild("LeftAntenna");
+		this.tail = body.getChild("Tail");
+		this.tail2 = tail.getChild("Tail2");
+		this.fin = tail2.getChild("Fin");
+		this.rightEye = body.getChild("RightEye");
+		this.leftEye = body.getChild("LeftEye");
+		this.rightBag = body.getChild("RightBag");
+		this.leftBag = body.getChild("LeftBag");
+		this.rightArm = body.getChild("RightArm");
+		this.rightHand = rightArm.getChild("RightHand");
+		this.leftArm = body.getChild("LeftArm");
+		this.leftHand = leftArm.getChild("LeftHand");
 	}
 
 	public static LayerDefinition createBodyLayer() {
@@ -61,11 +92,32 @@ public class CollectorModel extends EntityModel<Collector> {
 
 	@Override
 	public void setupAnim(Collector entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+		limbSwing = ageInTicks * 0.5F;
+		limbSwingAmount = 0.35F;
 
+		this.rightLeg.xRot = Mth.cos(limbSwing * 0.7F) * 1.4F * limbSwingAmount * 0.5F;
+		this.leftLeg.xRot = Mth.cos(limbSwing * 0.7F + (float)Math.PI) * 1.4F * limbSwingAmount * 0.5F;
+
+		this.rightAntenna.xRot = Mth.cos(limbSwing * 0.8F) * 0.4F * limbSwingAmount * 0.5F;
+		this.rightAntenna.zRot = Mth.cos(-1.0F + limbSwing * 0.8F) * 0.4F * limbSwingAmount * 0.5F + 0.2F;
+		this.leftAntenna.xRot = Mth.cos(limbSwing * 0.8F + (float)Math.PI) * 0.4F * limbSwingAmount * 0.5F;
+		this.leftAntenna.zRot = Mth.cos(-1.0F + limbSwing * 0.8F + (float)Math.PI) * 0.4F * limbSwingAmount * 0.5F - 0.2F;
+
+		this.tail.xRot = Mth.cos(limbSwing * 0.7F) * 0.25F * limbSwingAmount - 0.2F;
+		this.tail.zRot = Mth.cos(limbSwing * 0.35F) * 0.15F * limbSwingAmount;
+		this.fin.xRot = Mth.cos(limbSwing * 1.2F) * 0.5F * limbSwingAmount;
+
+		this.leftBag.y = Mth.cos(-3.0F + limbSwing * 0.85F) * limbSwingAmount * 1.0F + 2.0F;
+		this.rightBag.y = Mth.cos(-1.0F + limbSwing * 0.85F) * limbSwingAmount * 1.0F + 2.0F;
+
+		this.rightArm.xRot = Mth.cos(limbSwing * 0.7F) * limbSwingAmount * 0.3F - 0.45F;
+		this.rightHand.xRot = Mth.cos(-1.0F + limbSwing * 0.7F) * limbSwingAmount * 0.5F + 0.45F;
+		this.leftArm.xRot = Mth.cos(limbSwing * 0.7F + (float)Math.PI) * limbSwingAmount * 0.3F - 0.45F;
+		this.leftHand.xRot = Mth.cos(-1.0F + limbSwing * 0.7F + (float)Math.PI) * limbSwingAmount * 0.5F + 0.45F;
 	}
 
 	@Override
 	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
-		Body.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+		body.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
 	}
 }
--- src/main/java/coda/littlebeasties/common/entities/Collector.java
+++ src/main/java/coda/littlebeasties/common/entities/Collector.java
@@ -1,39 +1,148 @@
 package coda.littlebeasties.common.entities;
 
+import coda.littlebeasties.registry.LBItems;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.AgeableMob;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.Mob;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.stats.Stats;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.*;
+import net.minecraft.world.entity.monster.*;
 import net.minecraft.world.entity.npc.AbstractVillager;
+import net.minecraft.world.entity.npc.VillagerTrades;
+import net.minecraft.world.entity.npc.WanderingTrader;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.item.trading.MerchantOffer;
+import net.minecraft.world.item.trading.MerchantOffers;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
 public class Collector extends AbstractVillager {
+    private static final int NUMBER_OF_TRADE_OFFERS = 7;
 
     public Collector(EntityType<? extends AbstractVillager> collector, Level level) {
         super(collector, level);
     }
 
     public static AttributeSupplier.Builder createCollectorAttributes() {
-        return Mob.createMobAttributes().add(Attributes.MAX_HEALTH, 20.0D).add(Attributes.MOVEMENT_SPEED, 0.25D);
+        return Mob.createMobAttributes().add(Attributes.MAX_HEALTH, 20.0D).add(Attributes.MOVEMENT_SPEED, 0.3D);
+    }
+
+    protected void registerGoals() {
+        this.goalSelector.addGoal(0, new FloatGoal(this));
+        this.goalSelector.addGoal(0, new UseItemGoal<>(this, PotionUtils.setPotion(new ItemStack(Items.POTION), Potions.INVISIBILITY), SoundEvents.WANDERING_TRADER_DISAPPEARED, (p_35882_) -> {
+            return this.level.isNight() && !p_35882_.isInvisible();
+        }));
+        this.goalSelector.addGoal(0, new UseItemGoal<>(this, new ItemStack(Items.MILK_BUCKET), SoundEvents.WANDERING_TRADER_REAPPEARED, (p_35880_) -> {
+            return this.level.isDay() && p_35880_.isInvisible();
+        }));
+        this.goalSelector.addGoal(1, new TradeWithPlayerGoal(this));
+        this.goalSelector.addGoal(1, new AvoidEntityGoal<>(this, Zombie.class, 8.0F, 0.5D, 0.5D));
+        this.goalSelector.addGoal(1, new AvoidEntityGoal<>(this, Evoker.class, 12.0F, 0.5D, 0.5D));
+        this.goalSelector.addGoal(1, new AvoidEntityGoal<>(this, Vindicator.class, 8.0F, 0.5D, 0.5D));
+        this.goalSelector.addGoal(1, new AvoidEntityGoal<>(this, Vex.class, 8.0F, 0.5D, 0.5D));
+        this.goalSelector.addGoal(1, new AvoidEntityGoal<>(this, Pillager.class, 15.0F, 0.5D, 0.5D));
+        this.goalSelector.addGoal(1, new AvoidEntityGoal<>(this, Illusioner.class, 12.0F, 0.5D, 0.5D));
+        this.goalSelector.addGoal(1, new AvoidEntityGoal<>(this, Zoglin.class, 10.0F, 0.5D, 0.5D));
+        this.goalSelector.addGoal(1, new PanicGoal(this, 0.5D));
+        this.goalSelector.addGoal(1, new LookAtTradingPlayerGoal(this));
+        this.goalSelector.addGoal(4, new MoveTowardsRestrictionGoal(this, 0.35D));
+        this.goalSelector.addGoal(8, new WaterAvoidingRandomStrollGoal(this, 0.35D));
+        this.goalSelector.addGoal(9, new InteractGoal(this, Player.class, 3.0F, 1.0F));
+        this.goalSelector.addGoal(10, new LookAtPlayerGoal(this, Mob.class, 8.0F));
     }
 
     @Override
     protected void rewardTradeXp(MerchantOffer offer) {
-
+        if (offer.shouldRewardExp()) {
+            int i = 3 + this.random.nextInt(4);
+            this.level.addFreshEntity(new ExperienceOrb(this.level, this.getX(), this.getY() + 0.5D, this.getZ(), i));
+        }
     }
 
     @Override
     protected void updateTrades() {
+        VillagerTrades.ItemListing[] trades = VillagerTrades.WANDERING_TRADER_TRADES.get(1);
+        VillagerTrades.ItemListing[] trades1 = VillagerTrades.WANDERING_TRADER_TRADES.get(2);
+        if (trades != null && trades1 != null) {
+            MerchantOffers merchantoffers = this.getOffers();
+            this.addOffersFromItemListings(merchantoffers, trades, NUMBER_OF_TRADE_OFFERS);
+            int i = this.random.nextInt(trades1.length);
+            VillagerTrades.ItemListing listing = trades1[i];
+            MerchantOffer merchantoffer = listing.getOffer(this, this.random);
+            if (merchantoffer != null) {
+                merchantoffers.add(merchantoffer);
+            }
 
+        }
+    }
+
+    public boolean showProgressBar() {
+        return false;
+    }
+
+    public InteractionResult mobInteract(Player p_35856_, InteractionHand p_35857_) {
+        ItemStack itemstack = p_35856_.getItemInHand(p_35857_);
+        if (!itemstack.is(LBItems.COLLECTOR_SPAWN_EGG.get()) && this.isAlive() && !this.isTrading() && !this.isBaby()) {
+            if (p_35857_ == InteractionHand.MAIN_HAND) {
+                p_35856_.awardStat(Stats.TALKED_TO_VILLAGER);
+            }
+
+            if (!this.getOffers().isEmpty()) {
+                if (!this.level.isClientSide) {
+                    this.setTradingPlayer(p_35856_);
+                    this.openTradingScreen(p_35856_, this.getDisplayName(), 1);
+                }
+
+            }
+            return InteractionResult.sidedSuccess(this.level.isClientSide);
+        } else {
+            return super.mobInteract(p_35856_, p_35857_);
+        }
+    }
+
+    protected SoundEvent getAmbientSound() {
+        return this.isTrading() ? SoundEvents.WANDERING_TRADER_TRADE : SoundEvents.WANDERING_TRADER_AMBIENT;
+    }
+
+    protected SoundEvent getHurtSound(DamageSource p_35870_) {
+        return SoundEvents.WANDERING_TRADER_HURT;
+    }
+
+    protected SoundEvent getDeathSound() {
+        return SoundEvents.WANDERING_TRADER_DEATH;
+    }
+
+    protected SoundEvent getTradeUpdatedSound(boolean p_35890_) {
+        return p_35890_ ? SoundEvents.WANDERING_TRADER_YES : SoundEvents.WANDERING_TRADER_NO;
+    }
+
+    public SoundEvent getNotifyTradeSound() {
+        return SoundEvents.WANDERING_TRADER_YES;
+    }
+
+    @Override
+    public float getVoicePitch() {
+        return -5.0F;
     }
 
     @Nullable
     @Override
     public AgeableMob getBreedOffspring(ServerLevel level, AgeableMob mob) {
         return null;
+    }
+
+    @Override
+    public float getEyeHeight(Pose p_20237_) {
+        return 1.25F;
     }
 }
